5116a9b45faf98b4ea00ab8598f749f0af62f7eb
dushyant <dushyant@Dushyants-MacBook-Air.local>
2026-03-08 00:34:58 +0530

    enable CORS

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,21 @@
 from fastapi import FastAPI
 from app.routes import auth
 
+from fastapi.middleware.cors import CORSMiddleware
+
+origins = [
+    "http://localhost:5173",
+    "https://clara-test-v1.netlify.app"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app = FastAPI(title="CLARA API")
 
 app.include_router(auth.router, prefix="/auth")
