46fb8bdfcbcf3ff4e5b105aa28b07e23ec34fe2c
dushyant <dushyant@Dushyants-MacBook-Air.local>
2026-03-12 13:21:47 +0530

    Admin

--- backend/app/main.py
+++ backend/app/main.py
@@ -24,7 +24,7 @@ from app.routes import wishlist
 
 from app.database.db import engine
 from app.database.db import Base
-from app.routes import variants
+from app.routes.variants import router as variants_router
 
 
 
@@ -72,7 +72,7 @@ app.include_router(ai.router)
 app.include_router(reviews.router)
 app.include_router(upload.router)
 app.include_router(wishlist.router)
-app.include_router(variants.router)
+app.include_router(variants_router)
 
 
 @app.get("/")
