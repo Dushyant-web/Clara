957438b7ac06a743e004a2b90a68d844a53f30c1
dushyant <dushyant@Dushyants-MacBook-Air.local>
2026-03-12 23:44:02 +0530

    fix collections

--- backend/app/routes/categories.py
+++ backend/app/routes/categories.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from app.database.db import get_db
 from app.models.categories import Category
@@ -10,12 +10,18 @@ router = APIRouter()
 def get_categories(db: Session = Depends(get_db)):
     return db.query(Category).all()
 
+
 @router.get("/products/category/{slug}")
 def get_products_by_category(slug: str, db: Session = Depends(get_db)):
 
     category = db.query(Category).filter(Category.slug == slug).first()
 
     if not category:
-        return []
+        raise HTTPException(status_code=404, detail="Category not found")
 
-    return db.query(Product).filter(Product.category_id == category.id).all()
+    return (
+        db.query(Product)
+        .filter(Product.category_id == category.id)
+        .limit(12)
+        .all()
+    )
